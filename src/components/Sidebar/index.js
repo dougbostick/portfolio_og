d786f22342f84b9c3c5bc9e2a574298f55a28f2b
Doug Bostick <dougbostick@Dougs-MacBook.lan>
2023-08-07 15:01:06 -0400

    finished sidebar and styles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,7 +1,66 @@
 import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faHome, faEnvelope, faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Sidebar = () => {
-  return <>Hello from sidebar</>;
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <>LOGO</>
+      </Link>
+      <nav>
+        <NavLink
+          exact="true"
+          activeClassname="active"
+          className="home-link"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} />
+        </NavLink>
+
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/doug-bostick/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/dougbostick"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
